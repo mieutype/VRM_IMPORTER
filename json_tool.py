3539d534b8592263300dccb2662652ee39910fea
iCyP <tailefang+github@gmail.com>
2018-09-18 21:07:06 +0900

    json_toolでtkでファイル位置を聞くようにした

--- json_tool.py
+++ json_tool.py
@@ -6,40 +6,26 @@ https://opensource.org/licenses/mit-license.php
 """
 
 import json
-
-read_path = "./vroid\\Vroid.json"
+import tkinter
+import tkinter.filedialog
+read_path = tkinter.filedialog.askopenfilename()
 loaded_json =""
 with open(read_path,"rb") as f:
     loaded_json = json.load(f)
-print(loaded_json["scenes"])
-for id in loaded_json["scenes"][0]["nodes"]:
-    print("scenes nodes {}".format(loaded_json["nodes"][id]["name"]))
-for i,node in enumerate(loaded_json["nodes"]):
-    if node["name"] == "HairOrigin":
-        print("HairOrigin id is {}".format(i))
-        print("HairOrigin has child :{}".format("children" in node))
-        print("HairOrigin has {}".format(node.keys()))
-print("node の列挙")
-import copy
-nodes_name_list = {i:node["name"] for i, node in enumerate(loaded_json["nodes"]) }
-nodes_name_list_copy = copy.deepcopy(nodes_name_list)
-from pprint import pprint
-pprint(nodes_name_list)
-
-for i,node in enumerate(loaded_json["nodes"]):
-    if "children" in node:
-        if 151 in node["children"]:
-            print(i)
-
-print("--skinごとのjoints列挙--")
-for skin in loaded_json["skins"]:
-    for joint in skin["joints"]:
-        try:
-            print(nodes_name_list.pop(joint))
-        except:
-            print(nodes_name_list_copy[joint])
-    print("")
-
-print("---残り物(skinのjointで使われなかったnode達、そもそもボーンなのか？)----")
-for nokori in nodes_name_list.values():
-    print(nokori)
+    search_list = ["Hairs",
+                    "J_Bip_C_Head",
+                    "J_Bip_C_Neck",
+                    "J_Bip_C_UpperChest",
+                    "J_Bip_C_Chest",
+                    "J_Bip_C_Spine",
+                    "J_Bip_C_Hips",
+                    "Position",
+                    "Global",
+                    ]
+    import numpy
+    sum = numpy.array([0.0,0.0,0.0])
+    for id,node in enumerate(loaded_json["nodes"]):
+        if node["name"] in search_list:
+            print("{} id:{} is {}".format(node["name"],id,node["translation"]))
+            sum += numpy.array(node["translation"])
+    print("{:.5f}, {:.5f}, {:.5f}".format(sum[0],sum[1],sum[2]))

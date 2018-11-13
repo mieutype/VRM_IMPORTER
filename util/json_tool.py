bc25203c90eb32983ddcf077ef8e20b14c0a52b7
iCyP <tailefang+github@gmail.com>
2018-11-13 15:08:15 +0900

    忘れ物を追記

--- util/json_tool.py
+++ util/json_tool.py
@@ -9,6 +9,7 @@ import json
 import tkinter
 import tkinter.filedialog
 import struct
+from collections import OrderedDict
 read_path = tkinter.filedialog.askopenfilename(filetypes=[("vrm,json","*.vrm;*.json")])
 loaded_json =""
 with open(read_path, "rb") as f:
@@ -18,7 +19,7 @@ with open(read_path, "rb") as f:
         magic = 12 #offset from header
         bi_size = struct.unpack("<I", bi[magic:magic+4])[0]
         magic = 20 #offset from header
-        loaded_json = json.loads(bi[magic:magic+bi_size].decode("utf-8"))
+        loaded_json = json.loads(bi[magic:magic+bi_size].decode("utf-8"),object_pairs_hook=OrderedDict)
     elif filetype =="json":
         loaded_json = json.load(f)
     else:

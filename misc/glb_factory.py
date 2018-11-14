a11f8eba3248c92f54009018dd1b70fddc8e5711
iCyP <tailefang+github@gmail.com>
2018-11-15 02:28:58 +0900

    development

--- misc/glb_factory.py
+++ misc/glb_factory.py
@@ -138,20 +138,28 @@ class Glb_obj():
 			bpy.ops.object.mode_set(mode='EDIT')
 			bm = bmesh.from_edit_mesh(mesh.data)
 
-			primitive_index_bin_dic = {id:b"" for id in range(len(self.json_dic["materials"]))}
-			mat_id_dic = {mat.name:id for id,mat in enumerate(self.json_dic["materials"])} #tmpolary used
-			material_slot_dic = {id:mat.name for mat in mesh.material_slots}  #tmpolary used
-			shape_bin_dic = {shape.name:b"" for shape in mesh.data.shape_keys.key_blocks[1:]}#0番目Basisは省く
+			#region tempolary_used
+			mat_id_dic = {mat.name:id for id,mat in enumerate(self.json_dic["materials"])} 
+			material_slot_dic = {id:mat.name for mat in mesh.material_slots} 
+			node_id_dic = {node["name"]:id for node in self.json_dic["nodes"]} 
+			def joint_id_from_node_name_solver(node_name):
+				node_id = node_id_dic[node_name]
+				skin_id = self.json_dic["skins"][0]["joints"].index(node_id)
+				return skin_id
+			v_group_name_dic = {id:vg.name for vg in mesh.vertex_groups}
 			fmin,fmax = sys.float_info.min,sys.float_info.max
-			shape_min_max_dic = {shape.name:[[fmax,fmax,fmax],[fmin,fmin,fmin]] for shape in mesh.data.shape_keys.key_blocks}
+			unique_vertex_id = 0
+			unique_vertex_id_dic = {} #loop verts id : base vertex id (uv違いを同じ頂点番号で管理されているので)
+			uvlayers_dic = {id:uvlayer.name for id,uvlayer in enumerate(mesh.uv_layers)}
+			#endregion  tempolary_used
+			primitive_index_bin_dic = OrderedDict({id:b"" for id in range(len(self.json_dic["materials"]))})
+			shape_bin_dic = OrderedDict({shape.name:b"" for shape in mesh.data.shape_keys.key_blocks[1:]})#0番目Basisは省く
+			shape_min_max_dic = OrderedDict({shape.name:[[fmax,fmax,fmax],[fmin,fmin,fmin]] for shape in mesh.data.shape_keys.key_blocks})
 			position_bin =b""
 			position_min_max = [[fmax,fmax,fmax],[fmin,fmin,fmin]]
 			normal_bin = b""
 			joints_bin = b""
 			weights_bin = b""
-			unique_vertex_id = 0
-			unique_vertex_id_dic = {} #loop verts id : base vertex id (uv違いを同じ頂点番号で管理されているので)
-			uvlayers_dic = {id:uvlayer.name for id,uvlayer in enumerate(mesh.uv_layers)}
 			texcord_bins = {id:b"" for id in uvlayers_dic.keys()}
 			f_vec4_packer = struct.Struct("<ffff")
 			f_vec3_packer = struct.Struct("<fff")
@@ -162,7 +170,7 @@ class Glb_obj():
 				for i in range(3):
 					minmax[0][i] = vert_location[i] if vert_location[i] < minmax[0][i] else minmax[0][i]
 					minmax[1][i] = vert_location[i] if vert_location[i] > minmax[1][i] else minmax[1][i]
-					return
+				return
 			for face in bm.faces:
 				#このへん絶対超遅い
 				for loop in face.loops:
@@ -170,21 +178,58 @@ class Glb_obj():
 						uv_layer = bm.loops.layers.uv[uvlayer_name]
 						uv = loop[uv_layer].uv
 						texcord_bins[id] += f_pair_packer(uv[0],-uv[1]) #blenderとglbのuvは上下逆
-					for shape_name,shape_bin in shape_bin_dic.items():
+					for shape_name,shape_bin in shape_bin_dic.items(): 
 						shape_layer = bm.verts.layers.shape[shape_name]
-						vert_location = axis_blender_to_glb(loop.vert[shape_layer])
+						vert_location = axis_blender_to_glb( [loop.vert[shape_layer][i] - loop.vert.co[i] for i in range(3)])
 						shape_bin += f_vec3_packer(vert_location)
 						min_max(shape_min_max_dic[shape_name,vert_location])
+					weights = [0.0, 0.0, 0.0, 0.0]
+					magic = 9999999
+					joints = [magic,magic,magic,magic]
+					for v_group in mesh.data.vertices[loop.vert.index].groups:						
+							weights.remove(3)
+							weights.insert(0,v_group.weight)
+							joints.remove(3)
+							joints.insert(0,joint_id_from_node_name_solver(v_group_name_dic[v_group.group]))
+					joints_bin += I_vec4_packer(joints)
+					weights_bin += f_vec4_packer(weights) 
 					vert_location = axis_blender_to_glb(loop.vert.co)
 					position_bin += f_vec3_packer(vert_location)
 					min_max(position_min_max,vert_location)
 					normal_bin += f_vec3_packer(axis_blender_to_glb(loop.vert.normal))
 					unique_vertex_id_dic[unique_vertex_id,vert.index]
-					primitive_index_dic[mat_id_dic[material_slot_dic[face.material_index]]] += I_scalar_packer(unique_vertex_id)
+					primitive_index_bin_dic[mat_id_dic[material_slot_dic[face.material_index]]] += I_scalar_packer(unique_vertex_id)
 				unique_vertex_id += 1
-			#DONE:position uv normal morph
-			#TODO JOINT:vec4 WEIGHT:vec4
-			#endregion
+			#DONE :index position, uv, normal, position morph,JOINT WEIGHT  
+			#TODO morph_normal, color...?
+			primitive_glbs_dic = OrderedDict({id:Glb_bin(index_bin,"SCALAR",GL_CONSTANS.UNSIGNED_INT,unique_vertex_id,None,self.glb_bin_collector)})
+			pos_glb = Glb_bin(position_bin,"VEC3",GL_CONSTANS.FLOAT,unique_vertex_id,position_min_max,self.glb_bin_collector)
+			nor_glb = Glb_bin(normal_bin,"VEC3",GL_CONSTANS.FLOAT,unique_vertex_id,None,self.glb_bin_collector)
+			uv_glbs = [
+				Glb_bin(texcood_bin,"VEC2",GL_CONSTANS.FLOAT,unique_vertex_id,None,self.glb_bin_collector)
+					for texcood_bin in texcord_bins.values()]
+			joints_glb = Glb_bin(joints_bin,"VEC4",GL_CONSTANS.UNSIGNED_INT,unique_vertex_id,None,self.glb_bin_collector)
+			weights_glb = Glb_bin(weights_bin,"VEC4",GL_CONSTANS.FLOAT,unique_vertex_id,None,self.glb_bin_collector)
+			morph_glbs = [Glb_bin(morph_pos_bin,"VEC3",GL_CONSTANS.FLOAT,unique_vertex_id,morph_minmax,self.glb_bin_collector) 
+						for morph_pos_bin,morph_minmax in zip(shape_bin_dic.values(),shape_min_max_dic.values())
+						]
+			primitive_list = []
+			for primitive_id,index_glb in primitive_glbs_dic.items():
+				primitive = {"mode":4}
+				primitive["material"] = primitive_id
+				primitive["indices"] = index_glb.accessor_id
+				primitive["attributes"] = {
+					"POSITION":pos_glb.accessor_id,
+					"NORMAL":nor_glb.accessor_id,
+					"JOINTS_0":joints_glb.accessor_id,
+					"WEIGHTS_0":weights_glb.accessor_id
+				}
+				primitive["attributes"].update({"TEXCOORD_{}".format(i):uv_glb for i,uv_glb in enumerate(uv_glbs)})
+				primitive["targets"]=[OrderedDict({"POSITION":morph_glb for morph_glb in morph_glbs})]
+				primitive["extras"] = {"targetNames":[shape.name for shape in shape_bin_dic.keys()]} 
+
+			
+			#endregion hell
 		return
 
 
